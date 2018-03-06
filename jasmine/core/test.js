893e6cb0d8410e8a01e4542903f46751b0871948
Alejandro Barrero <abpdesign@gmail.com>
2018-03-05 23:11:00 -0500

    Added edge cases

--- jasmine/core/test.js
+++ jasmine/core/test.js
@@ -2,10 +2,30 @@
 
 describe("Refectoring toFixed to use string", function() {
 
+    it("Recieve integer numbers", function() {
+        expect( toFixed(10, 2) ).toBe( "10.00" );
+        expect( toFixed(1023454, 2) ).toBe( "1023454.00" );
+        expect( toFixed(19455, 2) ).toBe( "19455.00" );
+    });
+
     it("Round numbers propertly", function() {
         expect( toFixed(0.615, 2) ).toBe( "0.62" );
         expect( toFixed(10.235, 2) ).toBe( "10.24" );
         expect( toFixed(1.005, 2) ).toBe( "1.01" );
+    });
+
+    it("Manage 1 decimal", function() {
+        expect( toFixed(12.2, 2) ).toBe( "12.20" );
+        expect( toFixed(12455.2, 2) ).toBe( "12455.20" );
+    });
+
+    it("Manage negative numbers", function() {
+        expect( toFixed(-9.2, 2) ).toBe( "-9.20" );
+        expect( toFixed(-123.25, 2) ).toBe( "-123.25" );
+    });
+
+    it("Manage exponential", function() {
+        expect( toFixed(1.23e+20, 2) ).toBe( "123000000000000000000.00" );
     });
 
 });
--- tofixed.js
+++ tofixed.js
@@ -1,8 +1,24 @@
 function toFixed (value, precision) {
+  
+  // expect( toFixed(12.2, 2) ).toBe( "12.20" );
+  // 12.2 => 1220
+  arrayValue = [];
+  if(value.toString().match(/[.]/)) {
+    var arrayValue = value.toString().split('.');  
+  } else {
+    return value.toFixed(precision);
+  }
 
   // Become the value an string an move two integers the '.' to the right.
-  var arrayValue = value.toString().split('.');
-  var reArrenge =  arrayValue[0] + arrayValue[1].substr(0, 2) + '.' + arrayValue[1].substr(2);
+
+  // 
+  if ( arrayValue[1].length !== 1 ) {
+    var reArrenge =  arrayValue[0] + arrayValue[1].substr(0, 2) + '.' + arrayValue[1].substr(2);
+  // 
+  } else {
+    var addZero = '0';
+    var reArrenge =  arrayValue[0] + arrayValue[1] + addZero;
+  }
 
   // Calculate the number and become it to number
   var fixer = (Math.round(Number(reArrenge))/100).toFixed(precision);
