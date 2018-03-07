1ab34b637a0a88145f8de9dbd0d5d7d13c5b86fc
Alejandro Barrero <abpdesign@gmail.com>
2018-03-07 15:45:18 -0500

    Added test cases to the js file. It is going to help to test faster in the broser console.

--- tofixed.js
+++ tofixed.js
@@ -1,27 +1,45 @@
 function toFixed (value, precision) {
   
-  // expect( toFixed(12.2, 2) ).toBe( "12.20" );
-  // 12.2 => 1220
   arrayValue = [];
+  // Detect decimal number
   if(value.toString().match(/[.]/)) {
     var arrayValue = value.toString().split('.');  
   } else {
     return value.toFixed(precision);
   }
 
-  // Become the value an string an move two integers the '.' to the right.
-
-  // 
+  // Check if number has only one decimal
   if ( arrayValue[1].length !== 1 ) {
     var reArrenge =  arrayValue[0] + arrayValue[1].substr(0, 2) + '.' + arrayValue[1].substr(2);
-  // 
+  // If has only one decimal add a zero 
   } else {
     var addZero = '0';
     var reArrenge =  arrayValue[0] + arrayValue[1] + addZero;
   }
 
-  // Calculate the number and become it to number
+  // Calculate the number and become it to string
   var fixer = (Math.round(Number(reArrenge))/100).toFixed(precision);
 
   return fixer;
 }
+
+//=================================
+// Tests
+//================================
+
+//-------------------------------
+// Case 1
+
+toFiex(0.615, 2); // '0.62'
+
+//-------------------------------
+// Case 2
+
+toFiex(10.235, 2); // '10.24'
+
+//-------------------------------
+// Case 3
+
+toFiex(1.005, 2); // 1.01
+
+
